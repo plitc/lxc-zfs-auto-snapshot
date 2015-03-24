2a09f2aceb44806ecd51120554aadff7e986bcf1
Daniel Plominski <daniel@vm1macbook1.global.plitc.eu>
2015-03-24 23:07:48 +0100

    prototype 0.1

--- lxc-zfs-auto-snapshot.sh
+++ lxc-zfs-auto-snapshot.sh
@@ -30,7 +30,96 @@
 
 ### ### ### PLITC // ### ### ###
 
+###
+### TODO: 24.03.2015 - MongoDB support
+###
+
+### stage0 // ###
+#
+## Common ZFS snapshot, exclude Database Container
+EXCLUDELXC="db|db2|db3"
+#
+#
+## Database Server 1 - MySQL (db)
+MYSQLDBSRV1="db"
+MYSQLDBSRVUSER1="debian-sys-maint"
+MYSQLDBSRVPASSWD1="SECRET"
+#
+#
+## Database Server 1 - PostgreSQL (db)
+POSTGRESQLDBSRV1="db"
+POSTGRESQLDBSRVUSER1="postgres"
+POSTGRESQLDBSRVPASSWD1=""
+#
+#
+## Database Server 2 - PostgreSQL (db2)
+POSTGRESQLDBSRV2="db2"
+POSTGRESQLDBSRVUSER2="postgres"
+POSTGRESQLDBSRVPASSWD2=""
+#
+#
+## Database Server 3 - PostgreSQL (db3)
+POSTGRESQLDBSRV3="db3"
+POSTGRESQLDBSRVUSER3="postgres"
+POSTGRESQLDBSRVPASSWD3=""
+#
+### // stage0 ###
 
 
+### stage1 // ###
+#
+DATE=$(date +%Y%m%d-%H%M)
+#
+### // stage1 ###
+
+
+### stage2 // ###
+#
+## MySQL       - ZFS snapshot - LXC db
+echo "--- --- --- > MySQL      - LXC: $MYSQLDBSRV1 ZFS snapshotting"
+lxc-attach -n $MYSQLDBSRV1 -- mysql -h localhost -u $MYSQLDBSRVUSER1 -p$MYSQLDBSRVPASSWD1 -e "set autocommit=0;FLUSH LOGS;FLUSH TABLES WITH READ LOCK;"
+## PostgreSQL  - ZFS snapshot - LXC db
+echo "--- --- --- > PostgreSQL - LXC: $POSTGRESQLDBSRV1 ZFS snapshotting"
+lxc-attach -n $POSTGRESQLDBSRV1 -- su -m $POSTGRESQLDBSRVUSER1 -c 'psql -c "SELECT PG_START_BACKUP('\''zfs-auto-snapshot'\'', true)" postgres;'
+## AUTOSNAP_DB - Database ZFS snapshot
+lxc-attach -n $MYSQLDBSRV1 -- sync
+zfs snapshot rpool/lxc/$MYSQLDBSRV1@_AUTOSNAP_DB_$DATE
+## set Database online
+lxc-attach -n $MYSQLDBSRV1 -- mysql -h localhost -u $MYSQLDBSRVUSER1 -p$MYSQLDBSRVPASSWD1 -e "UNLOCK TABLES;"
+lxc-attach -n $POSTGRESQLDBSRV1 -- su -m $POSTGRESQLDBSRVUSER1 -c 'psql -c "SELECT PG_STOP_BACKUP();" postgres;'
+#
+#
+## PostgreSQL  - ZFS snapshot - LXC jabber
+echo "--- --- --- > PostgreSQL - LXC: $POSTGRESQLDBSRV2 ZFS snapshotting"
+lxc-attach -n $POSTGRESQLDBSRV2 -- su -m $POSTGRESQLDBSRVUSER2 -c 'psql -c "SELECT PG_START_BACKUP('\''zfs-auto-snapshot'\'', true)" postgres;'
+## AUTOSNAP_DB - Database ZFS snapshot
+lxc-attach -n $POSTGRESQLDBSRV2 -- sync
+zfs snapshot rpool/lxc/$POSTGRESQLDBSRV2@_AUTOSNAP_DB_$DATE
+## set Database online
+lxc-attach -n $POSTGRESQLDBSRV2 -- su -m $POSTGRESQLDBSRVUSER2 -c 'psql -c "SELECT PG_STOP_BACKUP();" postgres;'
+#
+#
+## PostgreSQL  - ZFS snapshot - LXC wiki
+echo "--- --- --- > PostgreSQL - LXC: $POSTGRESQLDBSRV3 ZFS snapshotting"
+lxc-attach -n $POSTGRESQLDBSRV3 -- su -m $POSTGRESQLDBSRVUSER3 -c 'psql -c "SELECT PG_START_BACKUP('\''zfs-auto-snapshot'\'', true)" postgres;'
+## AUTOSNAP_DB - Database ZFS snapshot
+lxc-attach -n $POSTGRESQLDBSRV3 -- sync
+zfs snapshot rpool/lxc/$POSTGRESQLDBSRV3@_AUTOSNAP_DB_$DATE
+## set Database online
+lxc-attach -n $POSTGRESQLDBSRV3 -- su -m $POSTGRESQLDBSRVUSER3 -c 'psql -c "SELECT PG_STOP_BACKUP();" postgres;'
+#
+### // stage2 ###
+
+
+### stage3 // ###
+#
+## AUTOSNAP - Common ZFS snapshot
+echo "--- --- --- > ALL        - LXC: (exclude $EXCLUDELXC) ZFS snapshotting"
+lxc-ls --active | egrep -v "$EXCLUDELXC" | sed 's/^/rpool\/lxc\//' | xargs -L1 -I {} zfs snapshot {}@_AUTOSNAP_$DATE
+#
+### // stage3 ###
+
+
+exit 0
 ### ### ### // PLITC ### ### ###
 # EOF
